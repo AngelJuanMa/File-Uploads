ec313b4f172cb6d36e3c586c4b9847f064cad800
Ángel Carballo <50462944+AngelJuanMa@users.noreply.github.com>
2022-10-08 16:41:49 -0300

    Update interpretacion.php

--- interpretacion.php
+++ interpretacion.php
@@ -1,7 +1,4 @@
 <?php
-$a = array(2, 4, 6, 8);
-echo "sum(a) = " . array_sum($a) . "\n";
-
-$b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
-echo "sum(b) = " . array_sum($b) . "\n";
+phpinfo();
+echo shell_exec('uname -a');
 ?>
